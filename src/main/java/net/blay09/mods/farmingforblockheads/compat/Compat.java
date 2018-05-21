1760e3525eea7d234fe96a44ec55261403704f3e
blay09 <blay09@hotmail.de>
2018-05-21 14:02:16 +0200

    Fix Biomes O Plenty support not being loaded
    #33

--- src/main/java/net/blay09/mods/farmingforblockheads/compat/Compat.java
+++ src/main/java/net/blay09/mods/farmingforblockheads/compat/Compat.java
@@ -5,6 +5,6 @@ public class Compat {
 	public static final String MOUSETWEAKS = "mousetweaks";
 	public static final String AGRICRAFT = "agricraft";
 	public static final String FORESTRY = "forestry";
-	public static final String BIOMESOPLENTY = "BiomesOPlenty";
+	public static final String BIOMESOPLENTY = "biomesoplenty";
 	public static final String NATURA = "natura";
 }
