da57de6ae733429bc9af1245450c69fd6a047204
blay09 <blay09@hotmail.de>
2018-09-02 09:22:02 +0200

    Fix tea, coffee and mustard seeds not being in the market defaults
    #44

--- src/main/java/net/blay09/mods/farmingforblockheads/compat/HarvestcraftAddon.java
+++ src/main/java/net/blay09/mods/farmingforblockheads/compat/HarvestcraftAddon.java
@@ -9,67 +9,76 @@ import net.minecraft.util.ResourceLocation;
 
 public class HarvestcraftAddon {
 
-	private static final String KEY_SEEDS = "Pams Harvestcraft Seeds";
-	private static final String KEY_SAPLINGS = "Pams Harvestcraft Saplings";
-	private static final String[] SEEDS = new String[] {
-			"blackberry", "blueberry", "candleberry", "raspberry", "strawberry", "cactusfruit", "asparagus", "barley", "oats", "rye", "corn", "bambooshoot", "cantaloupe", "cucumber", "wintersquash", "zucchini", "beet", "onion", "parsnip", "peanut", "radish", "rutabaga", "sweetpotato", "turnip", "rhubarb", "celery", "garlic", "ginger", "spiceleaf", "tealeaf", "coffeebean", "mustardseeds", "broccoli", "cauliflower", "leek", "lettuce", "scallion", "artichoke", "brusselsprout", "cabbage", "spinach", "whitemushroom", "bean", "soybean", "bellpepper", "chilipepper", "eggplant", "okra", "peas", "tomato", "cotton", "pineapple", "grape", "kiwi", "cranberry", "rice", "seaweed", "curryleaf", "sesameseeds", "waterchestnut",
-	};
+    private static final String KEY_SEEDS = "Pams Harvestcraft Seeds";
+    private static final String KEY_SAPLINGS = "Pams Harvestcraft Saplings";
+    private static final String[] SEEDS = new String[]{
+            "blackberry", "blueberry", "candleberry", "raspberry", "strawberry", "cactusfruit", "asparagus", "barley",
+            "oats", "rye", "corn", "bambooshoot", "cantaloupe", "cucumber", "wintersquash", "zucchini", "beet", "onion",
+            "parsnip", "peanut", "radish", "rutabaga", "sweetpotato", "turnip", "rhubarb", "celery", "garlic", "ginger",
+            "spiceleaf", "tea", "coffee", "broccoli", "cauliflower", "leek", "lettuce", "scallion", "artichoke",
+            "brusselsprout", "cabbage", "spinach", "whitemushroom", "bean", "soybean", "bellpepper", "chilipepper",
+            "eggplant", "okra", "peas", "tomato", "cotton", "pineapple", "grape", "kiwi", "cranberry", "rice",
+            "seaweed", "curryleaf", "sesameseeds", "waterchestnut", "mustard"
+    };
 
-	private static final String[] SAPLINGS = new String[] {
-			"apple", "almond", "apricot", "avocado", "banana", "cashew", "cherry", "chestnut", "coconut", "date", "dragonfruit", "durian", "fig", "gooseberry", "grapefruit", "lemon", "lime", "mango", "nutmeg", "olive", "orange", "papaya", "peach", "pear", "pecan", "peppercorn", "persimmon", "pistachio", "plum", "pomegranate", "starfruit", "vanillabean", "walnut", "cinnamon", "maple", "paperbark"
-	};
+    private static final String[] SAPLINGS = new String[]{
+            "apple", "almond", "apricot", "avocado", "banana", "cashew", "cherry", "chestnut", "coconut", "date",
+            "dragonfruit", "durian", "fig", "gooseberry", "grapefruit", "lemon", "lime", "mango", "nutmeg", "olive",
+            "orange", "papaya", "peach", "pear", "pecan", "peppercorn", "persimmon", "pistachio", "plum", "pomegranate",
+            "starfruit", "vanillabean", "walnut", "cinnamon", "maple", "paperbark"
+    };
 
-	public HarvestcraftAddon() {
-		FarmingForBlockheadsAPI.registerMarketDefaultHandler(KEY_SEEDS, new MarketRegistryDefaultHandler() {
-			@Override
-			public void apply(ItemStack defaultPayment) {
-				apply(defaultPayment, 1);
-			}
+    public HarvestcraftAddon() {
+        FarmingForBlockheadsAPI.registerMarketDefaultHandler(KEY_SEEDS, new MarketRegistryDefaultHandler() {
+            @Override
+            public void apply(ItemStack defaultPayment) {
+                apply(defaultPayment, 1);
+            }
 
-			@Override
-			public void apply(ItemStack defaultPayment, int defaultAmount) {
-				for (String cropName : SEEDS) {
-					String seedName = cropName + "seeditem";
-					Item seedItem = Item.REGISTRY.getObject(new ResourceLocation(Compat.HARVESTCRAFT, seedName));
-					if (seedItem != null) {
-						FarmingForBlockheadsAPI.registerMarketEntry(new ItemStack(seedItem, defaultAmount), defaultPayment, FarmingForBlockheadsAPI.getMarketCategorySeeds());
-					}
-				}
-			}
+            @Override
+            public void apply(ItemStack defaultPayment, int defaultAmount) {
+                for (String cropName : SEEDS) {
+                    String seedName = cropName + "seeditem";
+                    Item seedItem = Item.REGISTRY.getObject(new ResourceLocation(Compat.HARVESTCRAFT, seedName));
+                    if (seedItem != null) {
+                        FarmingForBlockheadsAPI.registerMarketEntry(new ItemStack(seedItem, defaultAmount), defaultPayment, FarmingForBlockheadsAPI.getMarketCategorySeeds());
+                    }
+                }
+            }
 
-			@Override
-			public boolean isEnabledByDefault() {
-				return true;
-			}
+            @Override
+            public boolean isEnabledByDefault() {
+                return true;
+            }
 
-			@Override
-			public ItemStack getDefaultPayment() {
-				return new ItemStack(Items.EMERALD);
-			}
-		});
+            @Override
+            public ItemStack getDefaultPayment() {
+                return new ItemStack(Items.EMERALD);
+            }
+        });
 
-		FarmingForBlockheadsAPI.registerMarketDefaultHandler(KEY_SAPLINGS, new MarketRegistryDefaultHandler() {
-			@Override
-			public void apply(ItemStack defaultPayment) {
-				for (String treeName : SAPLINGS) {
-					String saplingName = treeName + "_sapling";
-					Item saplingItem = Item.REGISTRY.getObject(new ResourceLocation(Compat.HARVESTCRAFT, saplingName));
-					if (saplingItem != null) {
-						FarmingForBlockheadsAPI.registerMarketEntry(new ItemStack(saplingItem), defaultPayment, FarmingForBlockheadsAPI.getMarketCategorySaplings());
-					}
-				}
-			}
+        FarmingForBlockheadsAPI.registerMarketDefaultHandler(KEY_SAPLINGS, new MarketRegistryDefaultHandler() {
+            @Override
+            public void apply(ItemStack defaultPayment) {
+                for (String treeName : SAPLINGS) {
+                    String saplingName = treeName + "_sapling";
+                    Item saplingItem = Item.REGISTRY.getObject(new ResourceLocation(Compat.HARVESTCRAFT, saplingName));
+                    if (saplingItem != null) {
+                        FarmingForBlockheadsAPI.registerMarketEntry(new ItemStack(saplingItem), defaultPayment, FarmingForBlockheadsAPI.getMarketCategorySaplings());
+                    }
+                }
+            }
 
-			@Override
-			public boolean isEnabledByDefault() {
-				return true;
-			}
+            @Override
+            public boolean isEnabledByDefault() {
+                return true;
+            }
 
-			@Override
-			public ItemStack getDefaultPayment() {
-				return new ItemStack(Items.EMERALD);
-			}
-		});
-	}
+            @Override
+            public ItemStack getDefaultPayment() {
+                return new ItemStack(Items.EMERALD);
+            }
+        });
+    }
 
 }
