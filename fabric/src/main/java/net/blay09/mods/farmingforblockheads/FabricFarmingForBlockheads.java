afb7e3c8cdd6c4f0c01212fdf736b17e120f8914
blay09 <blay09@hotmail.de>
2021-11-15 22:23:24 +0100

    some fabric prep

--- fabric/src/main/java/net/blay09/mods/farmingforblockheads/FabricFarmingForBlockheads.java
+++ fabric/src/main/java/net/blay09/mods/farmingforblockheads/FabricFarmingForBlockheads.java
@@ -1,10 +1,11 @@
 package net.blay09.mods.farmingforblockheads;
 
+import net.blay09.mods.balm.api.Balm;
 import net.fabricmc.api.ModInitializer;
 
 public class FabricFarmingForBlockheads implements ModInitializer {
     @Override
     public void onInitialize() {
-        FarmingForBlockheads.initialize();
+        Balm.initialize(FarmingForBlockheads.MOD_ID, FarmingForBlockheads::initialize);
     }
 }
--- fabric/src/main/java/net/blay09/mods/farmingforblockheads/client/FabricFarmingForBlockheadsClient.java
+++ fabric/src/main/java/net/blay09/mods/farmingforblockheads/client/FabricFarmingForBlockheadsClient.java
@@ -1,11 +1,13 @@
 package net.blay09.mods.farmingforblockheads.client;
 
+import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.farmingforblockheads.FarmingForBlockheads;
 import net.fabricmc.api.ClientModInitializer;
 
 public class FabricFarmingForBlockheadsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        FarmingForBlockheadsClient.initialize();
+        BalmClient.initialize(FarmingForBlockheads.MOD_ID, FarmingForBlockheadsClient::initialize);
     }
 }
