bc43bb2a4d99b0662d27abd586177cd3515a5e34
blay09 <blay09@hotmail.de>
2021-12-05 21:39:18 +0100

    Update to Minecraft 1.18

--- fabric/src/main/java/net/blay09/mods/farmingforblockheads/compat/ModMenuIntegration.java
+++ fabric/src/main/java/net/blay09/mods/farmingforblockheads/compat/ModMenuIntegration.java
@@ -2,7 +2,7 @@ package net.blay09.mods.farmingforblockheads.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import net.blay09.mods.balm.fabric.config.FabricBalmConfig;
 import net.blay09.mods.farmingforblockheads.FarmingForBlockheadsConfigData;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -11,6 +11,6 @@ import net.fabricmc.api.Environment;
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(FarmingForBlockheadsConfigData.class, parent).get();
+        return FabricBalmConfig.getConfigScreen(FarmingForBlockheadsConfigData.class);
     }
 }
