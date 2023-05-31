2204c18cff2f5e116ae311676b1e440a6a475e3f
Lgmrszd <lgmrszd@gmail.com>
2023-05-31 22:58:07 +0300

    Change EMI version to 1.0.1, move and adapt EmiIntegration to shared subproject.

--- shared/src/main/java/net/blay09/mods/farmingforblockheads/compat/EmiIntegration.java
+++ shared/src/main/java/net/blay09/mods/farmingforblockheads/compat/EmiIntegration.java
@@ -1,10 +1,12 @@
 package net.blay09.mods.farmingforblockheads.compat;
 
+import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.widget.Bounds;
 import net.blay09.mods.farmingforblockheads.client.gui.screen.MarketScreen;
 
+@EmiEntrypoint
 public class EmiIntegration implements EmiPlugin {
     @Override
     public void register(EmiRegistry registry) {
