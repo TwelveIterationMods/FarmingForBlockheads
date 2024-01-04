b382566080f75ab1ce1715a9754fec8dafa828d1
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-01-04 23:01:46 +0100

    wip: remove imc api

--- forge/src/main/java/net/blay09/mods/farmingforblockheads/ForgeFarmingForBlockheads.java
+++ forge/src/main/java/net/blay09/mods/farmingforblockheads/ForgeFarmingForBlockheads.java
@@ -6,14 +6,11 @@ import net.blay09.mods.farmingforblockheads.client.FarmingForBlockheadsClient;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(FarmingForBlockheads.MOD_ID)
 public class ForgeFarmingForBlockheads {
     public ForgeFarmingForBlockheads() {
         Balm.initialize(FarmingForBlockheads.MOD_ID, FarmingForBlockheads::initialize);
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(FarmingForBlockheads.MOD_ID, FarmingForBlockheadsClient::initialize));
-
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(IMCHandler::processIMC);
     }
 }
--- neoforge/src/main/java/net/blay09/mods/farmingforblockheads/ForgeFarmingForBlockheads.java
+++ neoforge/src/main/java/net/blay09/mods/farmingforblockheads/ForgeFarmingForBlockheads.java
@@ -6,14 +6,11 @@ import net.blay09.mods.farmingforblockheads.client.FarmingForBlockheadsClient;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(FarmingForBlockheads.MOD_ID)
 public class ForgeFarmingForBlockheads {
     public ForgeFarmingForBlockheads() {
         Balm.initialize(FarmingForBlockheads.MOD_ID, FarmingForBlockheads::initialize);
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(FarmingForBlockheads.MOD_ID, FarmingForBlockheadsClient::initialize));
-
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(IMCHandler::processIMC);
     }
 }
