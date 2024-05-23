83bcba285acd11816fc19122d61cf34415f2165c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-23 15:32:46 +0200

    fix(1.20.6): Fix Forge builds

--- forge/src/main/java/net/blay09/mods/farmingforblockheads/ForgeFarmingForBlockheads.java
+++ forge/src/main/java/net/blay09/mods/farmingforblockheads/ForgeFarmingForBlockheads.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.farmingforblockheads;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
 import net.blay09.mods.farmingforblockheads.client.FarmingForBlockheadsClient;
 import net.minecraftforge.api.distmarker.Dist;
@@ -10,7 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(FarmingForBlockheads.MOD_ID)
 public class ForgeFarmingForBlockheads {
     public ForgeFarmingForBlockheads() {
-        Balm.initialize(FarmingForBlockheads.MOD_ID, FarmingForBlockheads::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(FarmingForBlockheads.MOD_ID, FarmingForBlockheadsClient::initialize));
+        Balm.initialize(FarmingForBlockheads.MOD_ID, EmptyLoadContext.INSTANCE, FarmingForBlockheads::initialize);
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(FarmingForBlockheads.MOD_ID, EmptyLoadContext.INSTANCE, FarmingForBlockheadsClient::initialize));
     }
 }
